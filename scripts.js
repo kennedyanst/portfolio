80491035e050596212018d622b560113edb6f3f3
kenny <keke.218@gmail.com>
2023-10-19 23:40:33 -0300

    .

--- scripts.js
+++ scripts.js
@@ -11,3 +11,23 @@ sr.reveal('.area-3', {
     rotate: {x:100, y:100, z:0},
     duration: 2000
 });
+
+sr.reveal('.area-4', {
+    rotate: {x:150, y:150, z:0},
+    duration: 2500
+});
+
+sr.reveal('.area-5', {
+    rotate: {x:0, y:0, z:190},
+    duration: 2000
+});
+
+sr.reveal('.area-6', {
+    rotate: {x:0, y:200, z:0},
+    duration: 2000
+});
+
+sr.reveal('.area-7', {
+    rotate: {x:100, y:0, z:0},
+    duration: 2000
+});
