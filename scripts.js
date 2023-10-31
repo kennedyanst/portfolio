0a40833b04f1228d131c4e7ff8cb42fff17ce816
kenny <keke.218@gmail.com>
2023-10-31 13:41:39 -0300

    mobile cert

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ sr.reveal('.area-1', {duration: 1500});
 
 sr.reveal('.area-2', {
     rotate: {x:0, y:80, z:0},
-    duration: 1500
+    duration: 2500
 });
 
 sr.reveal('.area-3', {
     rotate: {x:100, y:100, z:0},
-    duration: 2000
+    duration: 2500
 });
 
 sr.reveal('.area-4', {
@@ -19,15 +19,41 @@ sr.reveal('.area-4', {
 
 sr.reveal('.area-5', {
     rotate: {x:0, y:0, z:190},
-    duration: 2000
+    duration: 2500
 });
 
 sr.reveal('.area-6', {
     rotate: {x:0, y:200, z:0},
-    duration: 2000
+    duration: 2500
 });
 
 sr.reveal('.area-7', {
     rotate: {x:100, y:0, z:0},
-    duration: 2000
+    duration: 2500
 });
+
+
+sr.reveal('.area-8', {
+    origin: 'left',  // Define a origem da animação (pode ser 'top', 'bottom', 'right', 'left', etc.)
+    distance: '100px', // Define a distância inicial do elemento
+    duration: 1000,   // Define a duração da animação em milissegundos
+    delay: 0,         // Define um atraso antes da animação
+    reset: true      
+});
+
+
+sr.reveal('.area-9', {
+    origin: 'top',
+    distance: '100px',
+    duration: 1000,
+    delay: 200, // Atraso de 200ms
+    reset: true
+});
+
+
+sr.reveal('.area-10', {
+    origin: 'right',
+    distance: '100px',
+    duration: 1000,
+    delay: 400, // Atraso de 400ms
+    reset: true});
